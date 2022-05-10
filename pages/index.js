f7923629332497b4df0249c61999183ba17fd65a
je-boska <joneirikboska@gmail.com>
2022-05-10 13:32:18 +0200

    Replace blog posts with fullscreen video embed

--- pages/index.js
+++ pages/index.js
@@ -10,24 +10,20 @@ export default function Home({ posts }) {
   return (
     <div className='container'>
       <Head>
-        <title>Next + Contentful Starter</title>
+        <title>Petra Hermanova</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main>
-        <Header />
-        <div className='posts'>
-          {posts.map((p) => {
-            return (
-              <Post
-                key={p.publishDate}
-                date={p.publishDate}
-                image={p.heroImage.fields}
-                title={p.title}
-              />
-            );
-          })}
-        </div>
+        <iframe
+          width='560'
+          height='315'
+          src='https://www.youtube.com/embed/0TWZlkcX7wQ'
+          title='YouTube video player'
+          frameBorder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowFullScreen
+        ></iframe>
       </main>
     </div>
   );
