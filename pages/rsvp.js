cbf8292535fa6b2349ed7f0ca119e78e1959bb07
je-boska <joneirikboska@gmail.com>
2023-09-14 15:52:02 +0200

    Create RSVP page for listening event

---
+++ pages/rsvp.js
@@ -0,0 +1,13 @@
+import Layout from '@components/Layout';
+import React from 'react';
+
+export default function RSVP() {
+  return (
+    <iframe
+      className='w-full h-screen rsvp'
+      src='https://docs.google.com/forms/d/e/1FAIpQLSdutPGCeNbugZEU6QD7tHp8Iu_renGUCeKB6sllVgcrmbZlAQ/viewform?embedded=true'
+    >
+      Loading…
+    </iframe>
+  );
+}
