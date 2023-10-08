fa7a39920758b7cdef2ae3b290473493ce0a0064
je-boska <joneirikboska@gmail.com>
2023-10-08 18:56:45 +0200

    Add Imprint & ToS link to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <Layout title='Petra Hermanova | Contact'>
       <ul className='absolute top-1/2 -translate-y-1/2 w-full text-center text-white'>
-        <li>
+        <li className='pb-2'>
           <a
             href='https://www.instagram.com/petrahermanova/'
             target='_blank'
@@ -13,7 +13,10 @@ export default function Contact() {
             Instagram
           </a>
         </li>
-        <li>Email</li>
+        <li className='pb-2'>info@petrahermanova.com</li>
+        <li>
+          <a href='/imprint'>Imprint and Terms of Service</a>
+        </li>
       </ul>
     </Layout>
   );
