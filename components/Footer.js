75058ac60bf8ba19c6b3175c84cdd7d0d596da88
je-boska <joneirikboska@gmail.com>
2023-10-10 17:46:06 +0200

    Adjust footer padding

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className='fixed bottom-0 h-10 md:h-16 w-full bg-black md:bg-transparent flex justify-center md:justify-start'>
-      <div className='fixed uppercase text-white text-center flex p-4 gap-3'>
+    <footer className='fixed bottom-0 h-12 md:h-16 w-full bg-black md:bg-transparent flex items-center justify-center md:justify-start'>
+      <div className='fixed uppercase text-white text-center px-4 flex gap-3'>
         <a href='/shows'>live</a>
         <a href='/shop'>shop</a>
         <a href='/contact'>info</a>
